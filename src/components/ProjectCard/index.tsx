62dadd36f0c692e80308d4419b4b6b832eec8d8e
Juliano Krindges <julianokrindges@gmail.com>
2020-12-14 22:32:46 -0400

    fix resposividade section projects

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/image";
+// import Image from "next/image";
 
 import styles from "./styles.module.css";
 
@@ -10,14 +10,13 @@ interface Props {
 const ProjectCard = ({ imgSrc }: Props) => {
   return (
     <a href="#" className={styles.link}>
-      <div className={styles.container}>
-        <div className={styles.imgContainer}>
-          <img
-            src={require("../../../public/home_hero.jpg")}
-            alt="código java script"
-            width="500px"
-          />
-        </div>
+      <div className={styles.imgContainer}>
+        <img
+          src={require("../../../public/home_hero.jpg")}
+          alt="código java script"
+          width="500px"
+          className={styles.img}
+        />
       </div>
     </a>
   );
