b63d7df1b4edbc355dfa884a15a730ff3ab86ffb
Juliano Krindges <julianokrindges@gmail.com>
2020-12-15 22:52:37 -0400

    fix main section responsivity

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,20 +4,22 @@ import styles from "./Styles.module.css";
 
 export default function Header() {
   return (
-    <header className={styles.container}>
-      <div>
-        <h2 className={styles.logo}>Juliano Krindges</h2>
-      </div>
-      <div className={styles.iconContainer}>
-        <a href="https://github.com/KrindgesJuliano">
-          <FiGithub color="white" size={30} />
-        </a>
-        <a href="https://www.linkedin.com/in/juliano-krindges-m-pinheiro-ab526b52/">
-          <FiLinkedin color="white" size={30} />
-        </a>
-        <a href="#">
-          <FiCodepen color="white" size={30} />
-        </a>
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div>
+          <h2 className={styles.logo}>Juliano Krindges</h2>
+        </div>
+        <div className={styles.iconContainer}>
+          <a href="https://github.com/KrindgesJuliano">
+            <FiGithub color="white" size={30} />
+          </a>
+          <a href="https://www.linkedin.com/in/juliano-krindges-m-pinheiro-ab526b52/">
+            <FiLinkedin color="white" size={30} />
+          </a>
+          <a href="#">
+            <FiCodepen color="white" size={30} />
+          </a>
+        </div>
       </div>
     </header>
   );
--- src/components/MainSection/index.tsx
+++ src/components/MainSection/index.tsx
@@ -10,20 +10,7 @@ interface Props {}
 export const MainSection = (props: Props) => {
   return (
     <SectionLayout className={styles.section}>
-      <svg width="0" height="0">
-        <defs>
-          <clipPath id="svgPath">
-            <path
-              fill="#FFFFFF"
-              stroke="#000000"
-              // stroke-width="1.5794"
-              // stroke-miterlimit="10"
-              d="M271.38 0H688.508V840H178.626L23.8596 629.395C-10.5918 590.179 
-              -1.58135 549.51 12.7292 518.04C27.0398 486.571 271.38 0 271.38 0Z"
-            />
-          </clipPath>
-        </defs>
-      </svg>
+      <div className={styles.backgroundContainer}></div>
       <div className={styles.sectionContainer}>
         <div className={styles.leftContainer}>
           <div className={styles.textContainer}>
@@ -55,14 +42,6 @@ export const MainSection = (props: Props) => {
               </a>
             </div>
           </nav>
-        </div>
-        <div className={styles.imageHeroContainer}>
-          <img
-            src={require("../../../public/home_hero.jpg")}
-            alt="código java script"
-            width="1500px"
-            height="100%"
-          />
         </div>
       </div>
     </SectionLayout>
